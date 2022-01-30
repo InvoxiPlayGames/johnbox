ced60ce8bdbb92570b62c018db475f48773a74c9
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2022-01-30 04:03:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Johnbox
+
+A private server implementation in NodeJS for modern Jackbox Games services (Ecast / API v2).
+
+Currently, this server only supports hosting 1 room at a time.
+
+**This project is not related to or endorsed by Jackbox Games, Inc.**
+
+## Supported Software
+
+### Tested known working games:
+
+* The Jackbox Party Pack 7
+    * Quiplash 3
+    * Champ'd Up
+* Drawful 2 International
+
+### Tested known non-working games:
+
+* The Jackbox Party Pack 8
+    * Job Job (UI on the frontend is bugged, can not progress)
+* Quiplash 2 InterLASHional
+    * Quiplash 2 InterLASHional only uses API v2 for gathering server info. Rooms are still handled via blobcast.
+* The Jackbox Party Pack 6
+    * All games in Party Pack 6 only use API v2 for gathering server info. Rooms are still handled via blobcast.
+* All games prior use Blobcast / API v1 (likely not the true names), which uses socketio for WebSockets and is currently not supported. 
+
+## Unimplemented features
+
+* Object security
+* Multiple rooms
+* Client reconnection
+* Room passcodes
+* Audiences
+* Moderation features
+* Blobcast / API v1(? what is the real name)
+
+## Usage
+
+This is **NOT** meant to be used in any form of serious environment. This is an experimental testing server, and there is no way to safely configure a web browser to connect to this server currently.
+
+1. `npm install ws` to install the WebSockets NodeJS module.
+2. Generate a TLS certificate for the web server to use.
+3. Edit the top of `johnbox.js` to change accessibleHost to a host accessible by all players (e.g. public IP)
+    * This must be
+4. `node johnbox` to start the server.
+5. Redirect the game to connect to your server. `jbg.config.jet` in each minigame folder has a `serverUrl` parameter.
---
+++ johnbox.js
@@ -0,0 +1,269 @@
+/*
+    Johnbox - Jackbox Games Private Server Implementation
+    Copyright (C) 2022 InvoxiPlayGames
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+var fs = require("fs");
+var https = require("https");
+var wslib = require("ws");
+var URL = require("url");
+
+// the host that the custom server is accessible with
+var accessibleHost = "127.0.0.1";
+// the TLS certificate / key used for the server
+// must have valid SNI for accessibleHost
+var tlsCertificate = "../localhost.crt";
+var tlsKey = "../localhost.key";
+// the room code to automatically assign to rooms
+var roomCode = "JOHN";
+
+var appConfigResponse = {
+    "ok": true,
+    "body": {
+        "settings": {
+            "serverUrl": accessibleHost
+        }
+    }
+}
+var roomsResponse = {
+    "ok": true,
+    "body": {
+        "host": accessibleHost,
+        "code": roomCode,
+        "token": "000000000000000000000000"
+    }
+}
+var appTag = "";
+var appID = "";
+var joinRoomsResponse = {
+    "ok": true,
+    "body": {
+        "appId": "",
+        "appTag": "",
+        "audienceEnabled": false,
+        "code": roomCode,
+        "host": accessibleHost,
+        "audienceHost": accessibleHost,
+        "locked": false,
+        "full": false,
+        "moderationEnabled": false,
+        "passwordRequired": false,
+        "twitchLocked": false,
+        "locale": "en",
+        "keepalive": false
+    }
+}
+var genericResponse = {
+    "ok": true,
+    "body": {}
+}
+var oldRoomResponse = {"create":true,"server":accessibleHost}
+
+function httpHandle(req, res) {
+    console.log('HTTP: ' + req.url);
+    // Ecast room create
+    if (req.method == 'POST' && req.url == '/api/v2/rooms') {
+        var data = "";
+        req.on('data', (chunk) => { data+=chunk; });
+        req.on('end', () => {
+            data = JSON.parse(data);
+            console.log(data);
+            appID = data.appId;
+            appTag = data.appTag;
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(roomsResponse));
+        });
+    // Ecast app configurations
+    } else if (req.url.startsWith('/api/v2/app-configs/')) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(appConfigResponse));
+    // Ecast room join
+    } else if (req.url.startsWith('/api/v2/rooms/')) {
+        res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+        joinRoomsResponse.body.appTag = appTag;
+        joinRoomsResponse.body.appId = appID;
+        res.end(JSON.stringify(joinRoomsResponse));
+    // Unknown
+    } else {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(genericResponse));
+    }
+}
+
+const server = https.createServer({
+    cert: fs.readFileSync(tlsCertificate),
+    key: fs.readFileSync(tlsKey)
+}, httpHandle);
+
+const wss = new wslib.WebSocketServer({ server });
+
+wss.on('connection', function connection(ws, request) {
+    console.log('WS: ' + request.url);
+    if (request.url.includes("role=host")) HostWSHandler(ws);
+    if (request.url.includes("role=player")) GuestWSHandler(ws, request.url);
+});
+
+server.listen(443);
+
+var roomObjects = {};
+var presenceMap = {};
+var playerCount = 0;
+var hostPC = 2;
+var guestPC = [];
+
+function HostWSHandler(ws) {
+    playerCount = 0;
+    presenceMap = {};
+    roomObjects = {};
+    presenceMap[1] = { socket: ws, id: "1", roles: { host: {} } };
+    ws.on('message', function message(data) {
+        var parsed = JSON.parse(data);
+        var text = false;
+        switch (parsed.opcode) {
+            case "text/create":
+            case "text/update":
+            case "text/set":
+                text = true;
+            case "object/create":
+            case "object/update":
+            case "object/set": // setting / updating objects to be passed around to players
+                // TODO: implement acl (access control list?) / permissions - currently all objects are sent to everyone, breaking games like jobjob
+                roomObjects[parsed.params.key] = parsed.params.val;
+                console.log("Host modified object", parsed.params.key);
+                ws.send(JSON.stringify({ "pc": ++hostPC, "re": parsed.seq, "opcode": "ok", "result": {} }));
+                for(var i = 2; i <= playerCount + 1; i++) {
+                    presenceMap[i].socket.send(JSON.stringify({ "pc": ++guestPC[i - 2], "opcode": text ? "text" : "object", "result": parsed.params }))
+                }
+                break;
+                case "drop": // delete the room object
+                    delete roomObjects[parsed.params.key];
+                    console.log("Host dropped object", parsed.params.key);
+                    ws.send(JSON.stringify({ "pc": ++hostPC, "re": parsed.seq, "opcode": "ok", "result": {} }));
+                    break;
+                case "room/get-audience": // we don't support audiences, but just return 0
+                    ws.send(JSON.stringify({ "pc": ++hostPC, "re": parsed.seq, "opcode": "room/get-audience", "result": {"connections": 0} }));
+                    break;
+                case "room/start-audience": // start audience?, TODO
+                case "room/exit": // kick all players, TODO
+                default:
+                    // just ignore and return default OK
+                    console.log("Unimplemented host opcode", parsed.opcode, "with data", parsed);
+                    ws.send(JSON.stringify({ "pc": ++hostPC, "re": parsed.seq, "opcode": "ok", "result": {} }));
+                    break;
+        }
+    });
+    // send welcome message
+    console.log("Sending host welcome.");
+    ws.send(JSON.stringify({
+        "pc": ++hostPC,
+        "opcode": "client/welcome",
+        "result": {
+            "id":1,
+            "secret":"000000000000000000000000",
+            "reconnect":false,
+            "deviceId":"0000000000.0000000000000000000000",
+            "entities":{},
+            "here":{},
+            "profile":null
+        }
+    }));
+}
+
+function GuestWSHandler(ws, url) {
+    guestPC.push(2);
+    var playerID = playerCount;
+    var username = URL.parse(url, true).query["name"] ? URL.parse(url, true).query["name"] : `JOHN ${playerID + 1}`;
+    playerCount++;
+    presenceMap[1 + playerCount] = { socket: ws, id: `${1+playerCount}`, roles: { player: { name: username } } };
+    ws.on('message', function message(data) {
+        var parsed = JSON.parse(data);
+        var text = false;
+        switch (parsed.opcode) {
+            case "text/create":
+            case "text/update":
+            case "text/set":
+                text = true;
+            case "object/create":
+            case "object/update":
+            case "object/set": // setting / updating objects to be passed around to players
+                // TODO: implement permissions checks
+                roomObjects[parsed.params.key] = parsed.params.val;
+                console.log("Client", 2 + playerID, "modified object", parsed.params.key);
+                presenceMap[1].socket.send(JSON.stringify({ "pc": ++hostPC, "opcode": text ? "text" : "object", "result": parsed.params }));
+                ws.send(JSON.stringify({ "pc": ++guestPC[playerID], "re": parsed.seq, "opcode": "ok", "result": {} }));
+                break;
+            case "client/send":
+                console.log("Client", parsed.params.from, "sent data to", parsed.params.to, ":", parsed.params.body);
+                // send it to host, TODO: check how this is supposed to behave
+                presenceMap[1].socket.send(JSON.stringify({ "pc": ++hostPC, "opcode": "client/send", "result": parsed.params }));
+                ws.send(JSON.stringify({ "pc": ++guestPC[playerID], "re": parsed.seq, "opcode": "ok", "result": {} }));
+                break;
+            default:
+                // just ignore and return default OK
+                console.log("Unimplemented client opcode", parsed.opcode, "with data", parsed);
+                ws.send(JSON.stringify({ "pc": ++guestPC[playerID], "re": parsed.seq, "opcode": "ok", "result": {} }));
+                break;
+        }
+    });
+    // send welcome message
+    console.log("Sending client welcome.");
+    var clientWelcome = {
+        "pc": ++guestPC[playerID],
+        "opcode": "client/welcome",
+        "result": {
+                "id": 1 + playerCount,
+                "secret":"00000000-0000-0000-0000-000000000000",
+                "reconnect":false,
+                "deviceId":"0000000000.0000000000000000000000",
+                "entities":{},
+                "here":{},
+                "profile":{
+                    "id": 1 + playerCount,
+                    "roles": {
+                        "player": { "name": username }
+                    }
+                }
+        }
+    };
+    for(var i = 1; i <= playerCount; i++) {
+        clientWelcome.result.here[`${i}`] = presenceMap[i];
+    }
+    var objectKeys = Object.keys(roomObjects);
+    for(var i = 0; i < objectKeys.length; i++) {
+        clientWelcome.result.entities[objectKeys[i]] = [ "object", { key: objectKeys[i], val: roomObjects[objectKeys[i]], version: 0, from: 1 } ];
+    }
+    ws.send(JSON.stringify(clientWelcome));
+    // send client connected message
+    var clientConnected = {
+        "pc":++hostPC,
+        "opcode":"client/connected",
+        "result":{
+            "id": 1 + playerCount,
+            "userId":`00000000-0000-0000-0000-00000000000${playerCount}`,
+            "name":username,
+            "role":"player",
+            "reconnect":false,
+            "profile":
+            {
+                "id": 1 + playerCount,
+                "roles": {
+                    "player": { "name": username }
+                }
+            }
+        }
+    };
+    presenceMap[1].socket.send(JSON.stringify(clientConnected));
+}
